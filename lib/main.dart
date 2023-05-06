0b9086a073e34ffa9a41692b13dedb94d18f7699
MinJing <goodcack@hotmail.com>
2023-05-06 16:01:56 +0800

    add firebase-cli and chatGPT support

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_minjing_stylish/bloc/application_bloc.dart';
 import 'package:flutter_minjing_stylish/page/home.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase with the default options for the current platform
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(Provider<ApplicationBloc>(
     create: (_) => ApplicationBloc(),
     child: MyApp(
