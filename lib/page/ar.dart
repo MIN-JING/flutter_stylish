8f708855605d5a19162ea56d502a42f28b63e322
MinJing <goodcack@hotmail.com>
2023-05-16 14:00:08 +0800

    failed to downgrade Java version

--- lib/page/ar.dart
+++ lib/page/ar.dart
@@ -1,6 +1,7 @@
-import 'package:vector_math/vector_math_64.dart';
-import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_minjing_stylish/page/screenshot.dart';
 
 class ARPage extends StatefulWidget {
   final String title;
@@ -17,23 +18,96 @@ class ARPage extends StatefulWidget {
 }
 
 class _ARPageState extends State<ARPage> {
-  late ARKitController arkitController;
+  String _platformVersion = 'Unknown';
+  static const String _title = 'AR Plugin Demo';
 
   @override
-  void dispose() {
-    arkitController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await ArFlutterPlugin.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      appBar: AppBar(title: const Text('ARKit in Flutter')),
-      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
-
-  void onARKitViewCreated(ARKitController arkitController) {
-    this.arkitController = arkitController;
-    final node = ARKitNode(
-        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
-    this.arkitController.add(node);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(_title),
+        ),
+        body: Column(children: [
+          Text('Running on: $_platformVersion\n'),
+          Expanded(
+            child: ExampleList(),
+          ),
+        ]),
+      ),
+    );
   }
+}
+
+class ExampleList extends StatelessWidget {
+  ExampleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final examples = [
+      Example(
+          'Screenshots',
+          'Place 3D objects on planes and take screenshots',
+              () => Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ScreenshotWidget()))),
+    ];
+    return ListView(
+      children:
+      examples.map((example) => ExampleCard(example: example)).toList(),
+    );
+  }
+}
+
+class ExampleCard extends StatelessWidget {
+  ExampleCard({Key? key, required this.example}) : super(key: key);
+  final Example example;
+
+  @override
+  build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        onTap: () {
+          example.onTap();
+        },
+        child: ListTile(
+          title: Text(example.name),
+          subtitle: Text(example.description),
+        ),
+      ),
+    );
+  }
+}
+
+class Example {
+  const Example(this.name, this.description, this.onTap);
+  final String name;
+  final String description;
+  final Function onTap;
 }
